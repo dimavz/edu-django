3a56b6231266bad66ff1def3916bea60d592939f
Dmitry <zatulenko@mail.ru>
2016-09-21 17:57:56 +0300

    Создано приложение Polls

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/dmitry/git_repositories/edu-django-git/edu-django/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.10.1','django-admin.py'
+__requires__ = 'Django==1.10.1'
+__import__('pkg_resources').run_script('Django==1.10.1', 'django-admin.py')
