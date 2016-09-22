21dde625b0a7430358fc3d22f55a046f433c18ff
Dmitry <zatulenko@mail.ru>
2016-09-22 18:00:47 +0300

    Доработка проекта

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'polls'
 
+"""
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
@@ -12,5 +13,12 @@ urlpatterns = [
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]"""
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
